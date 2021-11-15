813c14f3ceb8a13972362cbdb5989117c221165a
fanlino <fanlino@gmail.com>
2021-11-15 14:22:57 +0900

    Week 2-1 Commit

---
+++ week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/kr/dokim/layoutscodelab/MainActivity.kt
@@ -0,0 +1,50 @@
+package kr.dokim.layoutscodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import kr.dokim.layoutscodelab.ui.theme.LayoutsCodelabTheme
+import kotlin.math.max
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LayoutsCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                LayoutsCodelab()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LayoutsCodelabTheme {
+        LayoutsCodelab()
+    }
+}
